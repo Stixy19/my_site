d53f79ae43926bf2388ac57c4704cab10d3c6c0a
stixy19 <veta.dorohina@gmail.com>
2024-03-19 07:29:43 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const textContainer = document.querySelector('.text-container');
+const text = document.querySelector('.text');
+const toggleBtn = document.querySelector('.toggle-btn');
+
+toggleBtn.addEventListener('click', function () {
+    textContainer.classList.toggle('expanded');
+    if (textContainer.classList.contains('expanded')) {
+        toggleBtn.textContent = 'Свернуть';
+    } else {
+        toggleBtn.textContent = 'Развернуть';
+    }
+});
